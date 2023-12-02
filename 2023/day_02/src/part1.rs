28b638ad5a5dca999821df355d6a17a02cc0d785
seth <seth@ske.moe>
2023-12-02 02:44:47 -0600

    day two done

--- 2023/day_02/src/part1.rs
+++ 2023/day_02/src/part1.rs
@@ -1,10 +1,50 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
+fn parse_line(
+    line: &str
+) -> usize {
+    let num_red: usize = 12;
+    let num_green: usize = 13;
+    let num_blue: usize = 14;
+    let mut split_text = line.split(": ");
+    let game_id_str = split_text.next().unwrap();
+    let id: usize = game_id_str.split(' ').next_back().unwrap().parse().unwrap();
+    let game_contents = split_text.next().unwrap();
+    for round in game_contents.split("; ") {
+        let elements: Vec<&str> = round.split(", ").collect();
+        for element in elements {
+            let mut color_pair = element.split(' ');
+            let num_of_color = color_pair.next().unwrap().parse::<usize>().unwrap();
+            let color = color_pair.next().unwrap();
+            match color {
+                "red" => {
+                    if num_of_color > num_red {
+                        return 0;
+                    }
+                },
+                "green" => {
+                    if num_of_color > num_green {
+                        return 0;
+                    }
+                },
+                "blue" => {
+                    if num_of_color > num_blue {
+                        return 0;
+                    }
+                },
+                _ => unreachable!()
+            }
+        }
+    }
+    id
+}
+
+#[tracing::instrument]
 pub fn process(
-    _input: &str,
+    input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    Ok(format!("{}", input.lines().map(parse_line).sum::<usize>()))
 }
 
 #[cfg(test)]
@@ -13,9 +53,12 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!("8", process(input)?);
         Ok(())
     }
 }
--- 2023/day_02/src/part2.rs
+++ 2023/day_02/src/part2.rs
@@ -1,10 +1,48 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
+fn parse_line(
+    line: &str
+) -> usize {
+    let mut max_num_red: usize = 0;
+    let mut max_num_green: usize = 0;
+    let mut max_num_blue: usize = 0;
+    let split_text = line.split(": ");
+    let game_contents = split_text.last().unwrap();
+    for round in game_contents.split("; ") {
+        let elements: Vec<&str> = round.split(", ").collect();
+        for element in elements {
+            let mut color_pair = element.split(' ');
+            let num_of_color = color_pair.next().unwrap().parse::<usize>().unwrap();
+            let color = color_pair.next().unwrap();
+            match color {
+                "red" => {
+                    if num_of_color > max_num_red {
+                        max_num_red = num_of_color;
+                    }
+                },
+                "green" => {
+                    if num_of_color > max_num_green {
+                        max_num_green = num_of_color;
+                    }
+                },
+                "blue" => {
+                    if num_of_color > max_num_blue {
+                        max_num_blue = num_of_color;
+                    }
+                },
+                _ => unreachable!()
+            }
+        }
+    }
+    max_num_blue * max_num_green * max_num_red
+}
+
+#[tracing::instrument]
 pub fn process(
     input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    Ok(format!("{}", input.lines().map(parse_line).sum::<usize>()))
 }
 
 #[cfg(test)]
@@ -13,9 +51,12 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!("2286", process(input)?);
         Ok(())
     }
 }
