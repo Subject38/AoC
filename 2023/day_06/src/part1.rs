a79d4b8a6359ffe9c7d14a2e80d67d2ebfb7fa88
seth <seth@ske.moe>
2023-12-05 23:59:59 -0600

    day 6 complete

--- 2023/day_06/src/part1.rs
+++ 2023/day_06/src/part1.rs
@@ -1,10 +1,29 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let (times_str, distances_str) = input.split_once('\n').unwrap();
+    let (_, times) = times_str.split_once(':').unwrap();
+    let (_, distances) = distances_str.split_once(':').unwrap();
+    let times_vec = times
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<u32>>();
+    let distances_vec = distances
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<u32>>();
+    let cum_sum: u32 = times_vec.iter().zip(distances_vec).map(|(time, distance)| {
+        let mut num_ways = 0;
+        for i in 0..*time {
+            if i * (*time - i) > distance {
+                num_ways += 1;
+            }
+        }
+        num_ways
+    }).product();
+
+    Ok(format!("{}", cum_sum))
 }
 
 #[cfg(test)]
@@ -13,9 +32,10 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        // todo!("haven't built test yet");
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        assert_eq!("288", process(input)?);
         Ok(())
     }
 }
--- 2023/day_06/src/part2.rs
+++ 2023/day_06/src/part2.rs
@@ -1,10 +1,24 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let (times_str, distances_str) = input.split_once('\n').unwrap();
+    let (_, times) = times_str.split_once(':').unwrap();
+    let (_, distances) = distances_str.split_once(':').unwrap();
+    let mut time_s = times.to_owned();
+    time_s.retain(|c| !c.is_whitespace());
+    let mut distance_s = distances.to_owned();
+    distance_s.retain(|c| !c.is_whitespace());
+        
+    let time: u64 = time_s.parse().unwrap();
+    let distance: u64 = distance_s.parse().unwrap();
+    let first_time = (0..time).find(|i| {
+        i * (time - i) > distance
+    }).unwrap();
+    let last_time = (0..time).rev().find(|i| {
+        i * (time - i) > distance
+    }).unwrap();
+    Ok(format!("{}", last_time - first_time + 1))
 }
 
 #[cfg(test)]
@@ -13,9 +27,10 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        // todo!("haven't built test yet");
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        assert_eq!("71503", process(input)?);
         Ok(())
     }
 }
