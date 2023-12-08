0e0a8d64588a293709e132e20c7459c2a18ef42c
seth <seth@ske.moe>
2023-12-08 02:17:11 -0600

    comment out day 5 part 2 benchmarks so the suite can still run

--- 2023/day_05/benches/benchmarks-criterion.rs
+++ 2023/day_05/benches/benchmarks-criterion.rs
@@ -10,18 +10,18 @@ fn criterion_benchmark_part1(c: &mut Criterion) {
     group.finish();
 }
 
-fn criterion_benchmark_part2(c: &mut Criterion) {
-    let input = include_str!("../input.txt");
+// fn criterion_benchmark_part2(c: &mut Criterion) {
+//     let input = include_str!("../input.txt");
 
-    let mut group = c.benchmark_group("day_05::part2");
-    group.bench_with_input("part2", input, |b, input| b.iter(|| part2::process(input)));
+//     let mut group = c.benchmark_group("day_05::part2");
+//     group.bench_with_input("part2", input, |b, input| b.iter(|| part2::process(input)));
 
-    group.finish();
-}
+//     group.finish();
+// }
 
 criterion_group!(
     benches,
     criterion_benchmark_part1,
-    criterion_benchmark_part2
+    // criterion_benchmark_part2
 );
 criterion_main!(benches);
--- 2023/day_05/benches/benchmarks.rs
+++ 2023/day_05/benches/benchmarks.rs
@@ -10,7 +10,7 @@ fn part1() {
     part1::process(divan::black_box(include_str!("../input.txt",))).unwrap();
 }
 
-#[divan::bench]
-fn part2() {
-    part2::process(divan::black_box(include_str!("../input.txt",))).unwrap();
-}
+// #[divan::bench]
+// fn part2() {
+//     part2::process(divan::black_box(include_str!("../input.txt",))).unwrap();
+// }
