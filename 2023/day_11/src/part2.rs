24becb59859ebb2c3118b6517023e22c701b1d0d
seth <seth@ske.moe>
2023-12-12 21:28:30 -0600

    finish day 11, add benchmarks

--- 2023/day_11/src/part2.rs
+++ 2023/day_11/src/part2.rs
@@ -1,10 +1,73 @@
 use crate::custom_error::AocError;
+use itertools::Itertools;
+
+fn find_empty_rows(grid: &[&[char]]) -> Vec<usize> {
+    let mut res = Vec::new();
+    grid.iter().enumerate().for_each(|(index, row)| {
+        if !row.contains(&'#') {
+            res.push(index);
+        }
+    });
+    res
+}
+
+fn find_empty_cols(grid: &[&[char]]) -> Vec<usize> {
+    let mut res = Vec::new();
+    let num_cols = grid[0].len();
+    for col in 0..num_cols {
+        let mut found_galaxy = false;
+        for row in grid {
+            if !row.is_empty() && row[col] == '#' {
+                found_galaxy = true;
+            }
+        }
+        if !found_galaxy {
+            res.push(col)
+        }
+    }
+    res
+}
+
+fn get_real_coord(empty: &[usize], coord: usize) -> usize {
+    empty.iter().map(|empty_row_or_col| {
+        if coord > *empty_row_or_col {
+            return 999_999;
+        }
+        0
+    }).sum::<usize>() + coord
+}
+
+fn find_galaxy_locs(grid: &[&[char]], empty_cols: &[usize], empty_rows: &[usize]) -> Vec<(usize, usize)> {
+    let mut res = Vec::new();
+    grid.iter().enumerate().for_each(|(y, row)| {
+        row.iter().enumerate().for_each(|(x, c)| {
+            if *c == '#' {
+                let real_x = get_real_coord(empty_cols, x);
+                let real_y = get_real_coord(empty_rows, y);
+                res.push((real_y, real_x));
+            }
+        })
+    });
+    res
+}
 
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
+    input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    let input_chars = input.chars().collect::<Vec<char>>();
+    let grid = input_chars.split(|c| *c == '\n').collect::<Vec<_>>();
+    let empty_rows = find_empty_rows(&grid);
+    let empty_cols = find_empty_cols(&grid);
+    let galaxy_locs = find_galaxy_locs(&grid, &empty_cols, &empty_rows);
+    let sum = galaxy_locs.iter().combinations(2).map(|combination| {
+        let (y1, x1) = combination[0];
+        let (y2, x2) = combination[1];
+        let dist_x = if x2 > x1 { x2 - x1 } else { x1 - x2 };
+        let dist_y = if y2 > y1 { y2 - y1 } else { y1 - y2 };
+        dist_x + dist_y
+    }).sum::<usize>();
+    Ok(format!("{sum}"))
 }
 
 #[cfg(test)]
@@ -13,9 +76,17 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+        assert_eq!("82000210", process(input)?);
         Ok(())
     }
 }
