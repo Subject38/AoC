186994ca60f9c6711243caa9e46db7531c344420
seth <seth@ske.moe>
2023-12-12 21:57:32 -0600

    abandon day 11 for now. some problems are just a huge pain i think

--- 2023/day_12/src/part1.rs
+++ 2023/day_12/src/part1.rs
@@ -1,10 +1,21 @@
 use crate::custom_error::AocError;
 
+fn check_line_valid(line: &str) -> bool {
+    let (pattern, vals) = line.split_once(' ').expect("Should only be one space");
+    let mut springs = pattern.split('.').collect::<Vec<&str>>();
+    springs.retain(|&s| s != "");
+    let counts = vals.split(',').map(|s| s.parse().unwrap()).collect::<Vec<usize>>();
+    counts.is_empty()
+}
+
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
+    input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    for line in input.lines() {
+        check_line_valid(line);
+    }
+    Ok(format!("naww idek man. this problem is fr busted"))
 }
 
 #[cfg(test)]
@@ -13,9 +24,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+        assert_eq!("21", process(input)?);
         Ok(())
     }
 }
