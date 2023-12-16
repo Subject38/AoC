f5476ca43896de91fe15bfe5c36a2faff548065a
seth <seth@ske.moe>
2023-12-16 03:51:55 -0600

    cargo fmt

--- 2023/day_15/src/part1.rs
+++ 2023/day_15/src/part1.rs
@@ -1,18 +1,19 @@
 use crate::custom_error::AocError;
 
-fn hash(value: &str) -> u8 {
-    value.chars().fold(0_u8, |hash, c| {
-        hash.wrapping_add(c as u8).wrapping_mul(17)            
-    })
-}
-
 #[tracing::instrument]
-pub fn process(
-    input: &str,
-) -> miette::Result<String, AocError> {
-    let sum = input.lines().next().unwrap().split(',').map(|value| {
-        hash(value) as usize
-    }).sum::<usize>();
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let sum = input
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|value| {
+            value
+                .chars()
+                .fold(0_u8, |hash, c| hash.wrapping_add(c as u8).wrapping_mul(17))
+                as usize
+        })
+        .sum::<usize>();
     Ok(format!("{sum}"))
 }
 
