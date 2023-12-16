727128ebb1feb9aad65954fb442ff4999a5b9230
seth <seth@ske.moe>
2023-12-16 03:46:45 -0600

    finished part 1

--- 2023/day_15/src/part1.rs
+++ 2023/day_15/src/part1.rs
@@ -1,10 +1,19 @@
 use crate::custom_error::AocError;
 
+fn hash(value: &str) -> u8 {
+    value.chars().fold(0_u8, |hash, c| {
+        hash.wrapping_add(c as u8).wrapping_mul(17)            
+    })
+}
+
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
+    input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    let sum = input.lines().next().unwrap().split(',').map(|value| {
+        hash(value) as usize
+    }).sum::<usize>();
+    Ok(format!("{sum}"))
 }
 
 #[cfg(test)]
@@ -13,9 +22,9 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+";
+        assert_eq!("1320", process(input)?);
         Ok(())
     }
 }
