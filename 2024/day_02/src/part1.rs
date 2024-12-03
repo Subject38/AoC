39605820e5d21c962cfc4148b0e88d1af2a04e24
Subject38 <seth@ske.moe>
2024-12-02 21:18:57 -0600

    day 2 done

--- 2024/day_02/src/part1.rs
+++ 2024/day_02/src/part1.rs
@@ -1,8 +1,24 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let res = input.lines().fold(0, |acc, l| {
+        let mut line_iter = l.split_whitespace().peekable();
+        let mut prev_num: usize = line_iter.next().unwrap().parse().unwrap();
+        let increasing = line_iter.peek().unwrap().parse::<usize>().unwrap() > prev_num;
+        for item in line_iter {
+            let num: usize = item.parse().unwrap();
+            if increasing && (num <= prev_num || num - prev_num > 3) {
+                return acc;
+            }
+            if !increasing && (num >= prev_num || prev_num - num > 3) {
+                return acc;
+            }
+            prev_num = num
+        }
+        acc + 1
+    });
+    Ok(format!("{}", res))
 }
 
 #[cfg(test)]
--- 2024/day_02/src/part2.rs
+++ 2024/day_02/src/part2.rs
@@ -1,8 +1,39 @@
 use crate::custom_error::AocError;
-
+fn valid_line(line: &[i32]) -> bool {
+    let mut line_iter = line.iter().peekable();
+    let mut prev_num = line_iter.next().unwrap();
+    let increasing = line_iter.peek().unwrap() > &prev_num;
+    for item in line_iter {
+        let num = item;
+        if increasing && (num <= prev_num || num - prev_num > 3) {
+            return false;
+        }
+        if !increasing && (num >= prev_num || prev_num - num > 3) {
+            return false;
+        }
+        prev_num = num
+    }
+    true
+}
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let res = input.lines().fold(0, |acc, l| {
+        let line_vec = l
+            .split_ascii_whitespace()
+            .map(|val| val.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+        if valid_line(&line_vec) {
+            return acc + 1;
+        }
+        for i in 0..line_vec.len() {
+            // todo: optimize??? lmfao
+            if valid_line(&[&line_vec[0..i], &line_vec[i + 1..]].concat()) {
+                return acc + 1;
+            }
+        }
+        acc
+    });
+    Ok(format!("{}", res))
 }
 
 #[cfg(test)]
@@ -11,9 +42,15 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+1 3 2 3 5
+1 2 8 4 5";
+        assert_eq!("6", process(input)?);
         Ok(())
     }
 }
