d8bf9f39b9e7f6ff996219d279473d5fa8bd76f9
Subject38 <seth@ske.moe>
2024-12-10 23:47:36 -0600

    day 11 part 1 done. part 2 requires optimizing the algorithm somehow...

---
+++ 2024/day_11/src/part1.rs
@@ -0,0 +1,42 @@
+use crate::custom_error::AocError;
+
+fn blink(stones: &[usize]) -> Vec<usize> {
+    let mut res = Vec::with_capacity(stones.len() * 2);
+    for stone in stones {
+        let num_digits = if *stone == 0 { 1 } else { stone.ilog10() + 1 };
+        if *stone == 0 {
+            res.push(1)
+        } else if num_digits % 2 == 0 {
+            let h1 = stone / (10usize.pow(num_digits / 2));
+            let h2 = stone - h1 * 10usize.pow(num_digits / 2);
+            res.append(&mut vec![h1, h2])
+        } else {
+            res.push(stone.saturating_mul(2024))
+        }
+    }
+    res
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let mut stones: Vec<usize> = input
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    for _ in 0..25 {
+        stones = blink(&stones);
+    }
+    Ok(format!("{}", stones.len()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "125 17";
+        assert_eq!("55312", process(input)?);
+        Ok(())
+    }
+}
